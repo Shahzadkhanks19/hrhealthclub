5af9884caee7d4a36c8f6c0696ec3d31b52493e1
Shahzad Khan <joykhan78600@gmail.com>
2026-08-30 20:32:01 +0530

    refactor: use rebuilt homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,5 @@
-import { SiteHeader } from "@/components/site-header";
-import {
-  Coaches,
-  Community,
-  CtaBand,
-  FloatingActions,
-  Gallery,
-  Hero,
-  Programs,
-  Stats,
-  Testimonials,
-  Transformations,
-} from "@/components/home-sections";
-import { SiteFooter } from "@/components/site-footer";
+import { Homepage } from "@/components/homepage";
 
 export default function HomePage() {
-  return (
-    <main className="overflow-x-hidden">
-      <SiteHeader />
-      <Hero />
-      <Stats />
-      <Programs />
-      <Community />
-      <Coaches />
-      <Transformations />
-      <Gallery />
-      <Testimonials />
-      <CtaBand />
-      <SiteFooter />
-      <FloatingActions />
-    </main>
-  );
+  return <Homepage />;
 }
