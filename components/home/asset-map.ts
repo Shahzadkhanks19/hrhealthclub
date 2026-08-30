ee4035dd07dca41bef7b4c98e6d53222ecdb3013
Shahzad Khan <joykhan78600@gmail.com>
2026-08-30 21:07:43 +0530

    refactor: centralize stable homepage asset paths

---
+++ components/home/asset-map.ts
@@ -0,0 +1,7 @@
+export const homeAssets = {
+  logo: "/images/hr-logo.svg",
+  hero: "/images/hero.svg",
+  programMuscle: "/images/program-muscle.svg",
+  programFatLoss: "/images/program-fatloss.svg",
+  coach: "/images/coach-ravi.svg",
+} as const;
